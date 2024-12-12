e49f58879fcf7744b3b1b2faa3e8e26e79a59c7e
MagzhanSerikov <112800073+MagzhanSerikov@users.noreply.github.com>
2024-12-12 20:55:59 +0300

    Create field.h

---
+++ lab2/game_board/field.h
@@ -0,0 +1,31 @@
+#include "cell.h"
+
+class field
+{
+private:
+    uint32_t height_;
+    uint32_t width_;
+    cords entrance_;
+    cords exit_;
+    cell** cells_;
+    void allocate_field();
+    void deallocate_field();
+protected:
+public:
+    field(uint32_t h, uint32_t w, cords entrance = {1, 1});
+    field();
+    field(const field& other); 
+    field(field&& other) noexcept; 
+    field& operator=(const field& other);
+    field& operator=(field&& other) noexcept; 
+    ~field(); 
+
+    uint32_t get_height() const;
+    uint32_t get_width() const;
+    cords get_entrance() const;
+    cords get_exit() const;
+    void set_exit(cords exit_cords);
+    void set_type_of_cell(int x, int y, cell_type type);
+    cell_type touch_cell(int x, int y) const;
+    void str() const;
+};
