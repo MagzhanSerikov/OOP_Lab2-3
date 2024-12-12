7b90dad0f8b56957acfb59ed5c12429d989626f8
MagzhanSerikov <112800073+MagzhanSerikov@users.noreply.github.com>
2024-12-12 22:21:52 +0300

    Update controller.h

--- lab3/controller.h
+++ lab3/controller.h
@@ -2,15 +2,19 @@
 #include "field.h"
 #include "game_event.h"
 
+enum Direction {Top, Bottom, Right, Left};
+
 class Controller
 {
 private:
     player& p_;
     field& f_;
-
+protected:
+    cords cords_;
 public:
     Controller(player& p, field& f) : p_(p), f_(f) {}
-
+    cords get_cords_();
+    void move(Direction move_direction);
     void checkAndTriggerEvent(int x, int y)
     {
         GameEvent* event = f_.get_event_at(x, y);  
